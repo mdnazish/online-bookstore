1270ec9f9b0f713fa8493a924a1dffbd027b327c
Md Nazish <Md Nazish@MdNazish>
2020-03-23 16:04:06 +0530

    Initial project setup and configured application.properties file

---
+++ online-book-store/src/main/java/com/zitsoft/bookstore/OnlineBookStoresApplication.java
@@ -0,0 +1,13 @@
+package com.zitsoft.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoresApplication.class, args);
+	}
+
+}
