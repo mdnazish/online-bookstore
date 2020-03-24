29924b29b85b15768da721edc42724a038320cd4
Md Nazish <Md Nazish@MdNazish>
2020-03-24 11:57:07 +0530

    some changes in RepositoryConfig class

--- online-book-store/src/main/java/com/zitsoft/bookstore/config/RepositoryConfig.java
+++ online-book-store/src/main/java/com/zitsoft/bookstore/config/RepositoryConfig.java
@@ -13,20 +13,22 @@ import com.zitsoft.bookstore.entity.BookCategory;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
+	
 	@Autowired
 	private EntityManager entityManager;
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
-		// config.exposeIdsFor(new Class[] {Book.class,BookCategory.class}); // for specified classes only.
+		// config.exposeIdsFor(new Class[] {Book.class,BookCategory.class}); // for specific classes only.
 		
-		// For all entity classes.
+		// exposing ids in JSON response For all entity classes.
 		config.exposeIdsFor(
 				entityManager.getMetamodel()
 								.getEntities()
 								.stream()
 								.map(Type::getJavaType)
-								.toArray(Class[]::new));
+								.toArray(Class[]::new)
+		);
 	}
 }
